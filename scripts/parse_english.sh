3272b3dd633f118bf6fa3f97a05156c37204d410
Jiri Kerner <jiri.kerner@gmail.com>
2016-08-15 15:00:41 +0200

    adding czech syntaxnet model and scripts for model calling

---
+++ scripts/parse_english.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# To run on a conll formatted file, add the --conll command line argument.
+
+CDIR=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]})))
+PDIR=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]}))/..)
+cd ${PDIR}
+
+SYNTAXNET_HOME=${PDIR}
+BINDIR=${SYNTAXNET_HOME}/bazel-bin/syntaxnet
+
+PARSER_EVAL=${BINDIR}/parser_eval
+CONLL2TREE=${BINDIR}/conll2tree
+
+MODEL_DIR=${SYNTAXNET_HOME}/syntaxnet/models/parsey_mcparseface
+
+
+[[ "$1" == "--conll" ]] && INPUT_FORMAT=stdin-conll || INPUT_FORMAT=stdin
+
+$PARSER_EVAL \
+  --input=$INPUT_FORMAT \
+  --output=stdout-conll \
+  --hidden_layer_sizes=64 \
+  --arg_prefix=brain_tagger \
+  --graph_builder=structured \
+  --task_context=$MODEL_DIR/context.pbtxt \
+  --model_path=$MODEL_DIR/tagger-params \
+  --slim_model \
+  --batch_size=1024 \
+  --alsologtostderr \
+   | \
+  $PARSER_EVAL \
+  --input=stdin-conll \
+  --output=stdout-conll \
+  --hidden_layer_sizes=512,512 \
+  --arg_prefix=brain_parser \
+  --graph_builder=structured \
+  --task_context=$MODEL_DIR/context.pbtxt \
+  --model_path=$MODEL_DIR/parser-params \
+  --slim_model \
+  --batch_size=1024 \
+  --alsologtostderr 
