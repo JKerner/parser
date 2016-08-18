576fc27e0ea96a1952e56448d2cb6a99c9efa965
Jiri Kerner <jiri.kerner@gmail.com>
2016-08-18 11:47:30 +0200

    aa

--- appParse/parse_eng.sh
+++ appParse/parse_eng.sh
@@ -6,4 +6,5 @@
 ## TODO: not use sudo here, add docker to virtual group maybe? 
 sentence=$@
 #docker run syntaxnet "sh" "-c" "echo $sentence | bash parse_english.sh"
+cd /opt/tensorflow/models/syntaxnet
 echo $sentence | bash parse_english.sh
